7bc00ebf2691d9143cd3932169ab140129de207c
Laker Newhouse <lakernewhouse@gmail.com>
2022-01-21 13:30:34 -0800

    Knapsack problem example + performance increase

--- final_project/branch_and_bound.py
+++ final_project/branch_and_bound.py
@@ -22,6 +22,7 @@ import numpy as np
 import cvxpy as cp
 
 import queue
+import time
 
 # Input:  n by 1 vector c, k by n matrix A, k by 1 vector b
 # Output: n by 1 vector x with integer components which maximizes c^T x
@@ -29,7 +30,7 @@ def branch_and_bound(c, A, b):
     # Example element in the queue:
     #   [ (3, 4), (5, -2) ]
     # It means x_3 <= 4, x_5 >= 2 are additional constraints for this child.
-    q = queue.Queue()
+    q = queue.LifoQueue()
     l = -1 # lower bound for integer solutions
     int_best = np.zeros(c.size) # current best integer solution
 
@@ -82,6 +83,7 @@ def solve_relaxed_LP(c, A, b, additional_constraints):
         return None, None
     return x.value, l
 
+'''
 c = np.array([5, 8])
 A = np.array([
     [1, 1],
@@ -95,3 +97,19 @@ b = np.array([6, 45])
 #                x integer
 best_int_solution = branch_and_bound(c, A, b)
 print(best_int_solution)
+print(c.T @ best_int_solution)
+'''
+
+# knapsack problem (constraint to take only one of each item max)
+# items: apple, banana, Raisin Bran, Fiber One, chicken, beef, milk, wine
+budget = 40
+value = np.array([3,   9, 5, 6, 8, 2, 10,  1]) # how we value the items
+costs = np.array([1, 0.5, 5, 5, 9, 12, 5, 30])
+A = np.vstack((costs, np.identity(8)))
+b = np.hstack((budget, 5*np.ones(8)))
+
+start = time.time()
+best_int_solution = branch_and_bound(value, A, b)
+end = time.time()
+
+print(f'The best solution is to buy {best_int_solution}.\nThe cost is ${costs.T @ best_int_solution}.\nRuntime is {round(end - start, 3)} seconds.')
